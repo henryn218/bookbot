0ac0bd681c1cfa28e79186c638d9418878742cf8
henryn218 <henry.naish@gmail.com>
2024-09-04 09:20:42 +0100

    clean up

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def main():
     book_path = "books/frankenstein.txt"
-    with open(book_path) as f:
-        book = f.read()
+    book = get_book_text(book_path)
     word_count = count_words(book)
     character_counts = count_characters(book)
-    display_report(word_count, character_counts)
+    sorted_character_counts = sort_character_counts(character_counts)
+    display_report(word_count, sorted_character_counts)
 
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
 
 def count_words(text):
     words = text.split()
@@ -23,27 +26,28 @@ def count_characters(text):
                 character_counts[character] = 1
     return character_counts
 
+def sort_character_counts(character_counts):
+    characters = []
+    for character in character_counts:
+        characters.append({"name": character, "num": character_counts[character]})
+    characters.sort(reverse=True, key=sort_on)
+    return characters
 
 def sort_on(dict):
     return dict["num"]
 
 
-def display_report(word_count, character_counts):
-    characters = []
-    for character in character_counts:
-        characters.append({"name": character, "num": character_counts[character]})
-    characters.sort(reverse=True, key=sort_on)
-
+def display_report(word_count, sorted_character_counts):
     print(
         "\n--- Begin report of books/frankenstein.txt ---\n",
         f"\n{word_count} words found in the document\n",
     )
 
-    for character in characters:
+    for character in sorted_character_counts:
         print(
             f"The '{character["name"]}' character was found {character["num"]} times"
         )
     print("\n--- End report ---\n")
 
-
-main()
+if __name__ == "__main__":
+    main()
